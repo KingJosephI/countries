c4581c394c89c0fd873b374795032ae7df5c729b
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-09-30 17:01:59 +0100

    chore: install react-icons and axios dependencies

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Where in the world</h1>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
